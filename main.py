e887c51aff8ed672197db674a630cc30e96121d7
sareks1973255-ai <sareks1973255@gmail.com>
2026-05-11 05:35:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+import os
+
+API_ID = 20834477
+API_HASH = "08fe734379555d72c42399e1ba89ca68"
+SESSION = "f_QBvQA-Z4U"
+
+SOURCE_CHAT = -1001111111111
+TARGET_CHATS = [
+    -1002222222222,
+    -1003333333333
+]
+
+client = TelegramClient(
+    StringSession(SESSION),
+    API_ID,
+    API_HASH
+)
+
+@client.on(events.NewMessage(chats=SOURCE_CHAT))
+async def handler(event):
+    for chat in TARGET_CHATS:
+        try:
+            await client.forward_messages(
+                chat,
+                event.message
+            )
+            print(f"Forwarded to {chat}")
+        except Exception as e:
+            print(e)
+
+print("Bot ishga tushdi...")
+client.start()
+client.run_until_disconnected()
